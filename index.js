b49a39706472588cbcbdab650480e8999e2dbe8c
Saif-Rahman-801 <saifurs102@gmail.com>
2023-12-13 15:00:36 +0600

    cors option changed

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://the-grand-bonjour.web.app",
+      "http://localhost:5173",
+    ],
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.86h0qhu.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const database = client.db("TheGrandBonjour");
     const homePageImgCollection = database.collection("HomePageImg");
     const testimonialCollection = database.collection("Testimonials");
@@ -104,10 +111,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
